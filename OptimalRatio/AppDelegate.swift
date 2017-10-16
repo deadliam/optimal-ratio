27646991b1fba19d893efc1f74d69a04840d1dd0
Anatoly Kasyanov <tommy@macpaw.com>
2017-10-16 17:54:25 +0300

    Initial Commit

---
+++ OptimalRatio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OptimalRatio
+//
+//  Created by Anatoly Kasyanov on 10/16/17.
+//  Copyright © 2017 MacPaw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OptimalRatio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OptimalRatio
+//
+//  Created by Anatoly Kasyanov on 10/16/17.
+//  Copyright © 2017 MacPaw. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
